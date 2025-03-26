be2bd99dea409d2dc32020444ce54d1fa651281b
jungjoha <johann.jung@uni-a.de>
2025-03-26 14:08:35 +0100

    part 1/2 sprite animations

--- entities/player.lua
+++ entities/player.lua
@@ -4,32 +4,37 @@ Player = Entity.new(0, 0, nil)
 
 function Player:load()
 
-    local reference_sprite = love.graphics.newImage("assets/Player/Player_sprite-1.png")
-    local reference_height, local reference_width = reference_sprite:getDimensions()
+    local reference_sprite = love.graphics.newImage("assets/Player/Player_sprite_1.png")
+    local reference_width, reference_height = reference_sprite:getDimensions()
 
 
-    self.sprite = {
-        local image = love.graphics.newImage("assets/Player/Player_sprite")
-        sprite_batch = love.graphics.newSpriteBatch(image)
-        facing_forward = {}
-        facing_backward = {}
-        facing_right = {}
-        facing_left = {}
+    local image = love.graphics.newImage("assets/Player/Player_sprite_map.png")
+    self.sprite_batch = love.graphics.newSpriteBatch(image)
+    self.facing_forward = {}
+    self.facing_backward = {}
+    self.facing_right = {}
+    self.facing_left = {}
 
-        for y = 0, image:getHeight(), reference_height do
-            local quad_forward = love.graphics.newQuad(0, y, reference_sprite:getDimensions(), image:getDimensions())
-            local quad_backward = love.graphics.newQuad(reference_width, y, reference_sprite:getDimensions(), image:getDimensions())
-            local quad_right = love.graphics.newQuad(reference_width*2, y, reference_sprite:getDimensions(), image:getDimensions())
-            local quad_left = love.graphics.newQuad(reference_width*3, y, reference_sprite:getDimensions(), image:getDimensions())
-            local index = y / reference_height
-            facing_forward[index] = quad_forward
-            facing_backward[index] = quad_backward
-            facing_right[index] = quad_right
-            facing_left[index] = quad_left
-        end
+    local index = 1
+    for y = 0, image:getHeight(), reference_height do
+        
+        local quad_forward = love.graphics.newQuad(0, y, reference_width, reference_height, image:getDimensions())
+        local quad_backward = love.graphics.newQuad(reference_width, y, reference_width, reference_height, image:getDimensions())
+        local quad_right = love.graphics.newQuad(reference_width*2, y, reference_width, reference_height, image:getDimensions())
+        local quad_left = love.graphics.newQuad(reference_width*3, y, reference_width, reference_height, image:getDimensions())
+        local index = y / reference_height
+        self.facing_forward[index] = quad_forward
+        self.facing_backward[index] = quad_backward
+        self.facing_right[index] = quad_right
+        self.facing_left[index] = quad_left
 
-        animation_counter = 1
-    }
+        index = index + 1
+    end
+
+    self.animation_counter = 1
+    self.sprite = love.graphics.newQuad(0, 0, reference_width, reference_height, image:getDimensions())
+    self.animation_used = self.facing_forward
+
 
     self.max_health = 5
     self.current_health = self.max_health
@@ -38,8 +43,8 @@ function Player:load()
     self.movement_vector.vertical = 0
     self.movement_vector.horizontal = 0
 
-    self.width = self.sprite:getWidth()
-    self.height = self.sprite:getHeight()
+    self.width = reference_width
+    self.height = reference_height
     self.base_speed = 450
     self.score = 0
 end
@@ -53,7 +58,14 @@ function Player:update(dt)
 end
 
 function Player:draw()
-    
+    self.sprite_batch:clear()
+    self.sprite_batch:add(self.sprite)
+    love.graphics.draw(self.sprite_batch, self.x_position, self.y_position)
+    self.animation_counter = self.animation_counter + 1
+    if self.animation_counter  > #self.animation_used then
+        self.animation_counter = 1
+    end
+    Player:choose_animation()
 end
 
 function Player:check_out_of_bounds()
@@ -105,9 +117,22 @@ function Player:move(dt)
     self.movement_vector.vertical = 0
     self.movement_vector.horizontal = 0
 
-
-    self.sprite = self.sprites[self.current_animation]
 end
 
+
+function Player:choose_animation()
+    if self.current_animation == 1 then
+        self.animation_used = self.facing_forward
+    elseif self.current_animation == 2 then
+        self.animation_used = self.facing_backward
+    elseif self.current_animation == 3 then
+        self.animation_used = self.facing_right
+    elseif self.current_animation == 4 then
+        self.animation_used = self.facing_left
+    else
+        self.animation_used = self.facing_forward
+    end
+    self.sprite = self.animation_used[self.animation_counter]
+end
 
 
